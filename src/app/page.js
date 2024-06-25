20b033fcab8ede202872361e02514864e611b206
ssttm169 <royongbin@qq.com>
2024-06-25 13:11:11 +0800

    init

---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-bold text-3xl">橙云面试稿</h1>
+
+      <div>
+        <div>
+        👨🏻面试人：檀武
+        </div>
+
+        <div>
+        💡 微信号：tom169
+        </div>
+
+        
+        <div>
+         📩 Email：14665350@qq.com
+        </div>
+
+        <div>
+         📃<a href="/docs">docs链接</a>
+        </div>
+
+      </div>
+
+    </main>
+  );
+}
